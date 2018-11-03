80d52cad17dacf9f1c9b712b0302b419a5fe7312
Adam Nitido <anitido@g.harvard.edu>
2018-11-03 14:20:25 -0400

    created setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="AutoGrad",
+    version="0.0.1",
+    author="cs207-project-group4",
+    author_email="N/A",
+    description="Automatic Differentiation Library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cs207-project-group4/cs207-FinalProject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
