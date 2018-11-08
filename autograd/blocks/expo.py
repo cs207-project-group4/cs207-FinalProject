15149847b3947b484a6ce02a8741927988e4a578
Paxton <pmaederyork@gmail.com>
2018-11-08 17:01:39 -0500

    Started exp and expo test

---
+++ autograd/blocks/expo.py
@@ -0,0 +1,16 @@
+from autograd.blocks.block import SimpleBlock
+import numpy as np
+
+
+class exp(SimpleBlock):
+    """
+    vectorized sinus function on vectors
+    """
+
+    def data_fn(self, args):
+        new_data = np.exp(args.data)
+        return (new_data)
+
+    def gradient_fn(self, args):
+        grad = np.exp(args.data)
+        return (grad)
