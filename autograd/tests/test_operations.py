81abdb44927b08e1a9c7283bae7311489c9749df
Paxton <pmaederyork@gmail.com>
2018-11-08 18:52:56 -0500

    test_operations except power

--- autograd/tests/test_operations.py
+++ autograd/tests/test_operations.py
@@ -3,7 +3,6 @@ from autograd.blocks.operations import subtract
 from autograd.blocks.operations import multiply
 from autograd.blocks.operations import divide
 from autograd.blocks.operations import power
-from autograd.blocks.operations import dot
 
 from autograd.variable import Variable
 import numpy as np
@@ -39,3 +38,137 @@ def test_add():
 #   assert gradient forward pass
 # =============================================================================
     assert np.equal(gradient_true, y_block.gradient).all(), 'wrong add gradient forward pass. expected {}, given{}'.format(gradient_true,y_block.gradient)
+
+def test_subtract():
+# =============================================================================
+#   define the input variable
+# =============================================================================
+    datax=np.random.random(5)
+    datay=np.random.random(5)
+    x=Variable(datax)
+    y=Variable(datay)
+# =============================================================================
+#   define custom block
+# =============================================================================
+    subtract_block=subtract()
+
+# =============================================================================
+#   compute output of custom block
+# =============================================================================
+    y_block=subtract_block(x,y)
+# =============================================================================
+#   define expected output
+# =============================================================================
+    data_true=np.subtract(datax,datay)
+    gradient_true=np.zeros((5,5))
+# =============================================================================
+#   assert data pass
+# =============================================================================
+    assert np.equal(data_true, y_block.data).all(), 'wrong sub data pass. expected {}, given{}'.format(data_true, y_block.data)
+
+# =============================================================================
+#   assert gradient forward pass
+# =============================================================================
+    assert np.equal(gradient_true, y_block.gradient).all(), 'wrong sub gradient forward pass. expected {}, given{}'.format(gradient_true,y_block.gradient)
+
+def test_multiply():
+# =============================================================================
+#   define the input variable
+# =============================================================================
+    datax=np.random.random(5)
+    datay=np.random.random(5)
+    x=Variable(datax)
+    y=Variable(datay)
+# =============================================================================
+#   define custom block
+# =============================================================================
+    multiply_block=multiply()
+
+# =============================================================================
+#   compute output of custom block
+# =============================================================================
+    y_block=multiply_block(x,y)
+
+# =============================================================================
+#   define expected output
+# =============================================================================
+    data_true=np.multiply(datax,datay)
+    gradient_true=np.identity(5)*y.data + np.identity(5)*x.data
+# =============================================================================
+#   assert data pass
+# =============================================================================
+    assert np.equal(data_true, y_block.data).all(), 'wrong mult data pass. expected {}, given{}'.format(data_true, y_block.data)
+
+# =============================================================================
+#   assert gradient forward pass
+# =============================================================================
+    assert np.equal(gradient_true, y_block.gradient).all(), 'wrong mult gradient forward pass. expected {}, given{}'.format(gradient_true,y_block.gradient)
+
+def test_divide():
+# =============================================================================
+#   define the input variable
+# =============================================================================
+    datax=np.random.random(5)
+    datay=np.random.random(5)
+    x=Variable(datax)
+    y=Variable(datay)
+# =============================================================================
+#   define custom block
+# =============================================================================
+    divide_block=divide()
+
+# =============================================================================
+#   compute output of custom block
+# =============================================================================
+    y_block=divide_block(x, y)
+# =============================================================================
+#   define expected output
+# =============================================================================
+    data_true =np.divide(datax,datay)
+
+    gradient_true = (x.gradient*y.data-x.data*y.gradient)/y.data**2
+# =============================================================================
+#   assert data pass
+# =============================================================================
+    assert np.equal(data_true, y_block.data).all(), 'wrong div data pass. expected {}, given{}'.format(data_true, y_block.data)
+
+# =============================================================================
+#   assert gradient forward pass, use allclose because division create small differences.  Outputs look same though!
+# =============================================================================
+    assert np.allclose(gradient_true, y_block.gradient, rtol = .001), 'wrong div gradient forward pass. expected {}, given{}'.format(gradient_true,y_block.gradient)
+
+
+def test_power():
+# =============================================================================
+#   define the input variable
+# =============================================================================
+    datax=np.random.random(5)
+    x=Variable(datax)
+    y= 5 #np.random.randint(low=0,high=100)
+    print(x)
+# =============================================================================
+#   define custom block
+# =============================================================================
+    power_block=power()
+
+# =============================================================================
+#   compute output of custom block
+# =============================================================================
+    y_block=power_block(x,y)
+
+# =============================================================================
+#   define expected output power & product rule: (x^y)' = yx'x^(y-1)
+# =============================================================================
+    data_true=np.power(datax,y)
+    gradient_true = y*x.gradient*datax^(y-1)
+# =============================================================================
+#   assert data pass
+# =============================================================================
+    assert np.equal(data_true, y_block.data).all(), 'wrong div data pass. expected {}, given{}'.format(data_true, y_block.data)
+
+# =============================================================================
+#   assert gradient forward pass
+# =============================================================================
+    assert np.equal(gradient_true, y_block.gradient).all(), 'wrong div gradient forward pass. expected {}, given{}'.format(gradient_true,y_block.gradient)
+
+test_power()
