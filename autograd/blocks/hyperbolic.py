4036089e8c5b20421378d3eb712537790d2883ef
Paxton <pmaederyork@gmail.com>
2018-11-08 15:33:59 -0500

    added hyperbolic functions

---
+++ autograd/blocks/hyperbolic.py
@@ -0,0 +1,38 @@
+from  autograd.blocks.block import SimpleBlock
+import numpy as np
+
+class sinh(SimpleBlock):
+    """
+	vectorized cosine function on vectors
+	"""
+	def data_fn(self, args):
+		new_data = np.sinh(args.data)
+		return(new_data)
+
+	def gradient_fn(self, args):
+		grad = np.cosh(args.data)
+		return(grad)
+
+class cosh(SimpleBlock):
+	"""
+	vectorized cosine function on vectors
+	"""
+	def data_fn(self, args):
+		new_data = np.cosh(args.data)
+		return(new_data)
+
+	def gradient_fn(self, args):
+		grad = np.sinh(args.data)
+		return(grad)
+
+class tanh(SimpleBlock):
+    """
+    vectorized cosine function on vectors
+	"""
+    def data_fn(self, args):
+        new_data = np.tanh(args.data)
+        return(new_data)
+
+    def gradient_fn(self, args):
+        grad = 1 - tanh(args.data)**2
+        return(grad)
---
+++ autograd/tests/test_hyperbolic.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from autograd.blocks.trigo import sin
+from autograd.variable import Variable
+import numpy as np
+
+def test_sin():
+# =============================================================================
+#   define the input variable
+# =============================================================================
+    data=np.random.random(5)
+    x=Variable(data)
+
+# =============================================================================
+#   define custom block
+# =============================================================================
+    sin_block=sin()
+
+# =============================================================================
+#   compute output of custom block
+# =============================================================================
+    y_block=sin_block(x)
+
+# =============================================================================
+#   define expected output
+# =============================================================================
+    data_true=np.sin(data)
+    gradient_true=np.diag(np.cos(data))
+
+# =============================================================================
+#   assert data pass
+# =============================================================================
+    assert np.equal(data_true, y_block.data).all(), 'wrong data pass. expected {}, given{}'.format(data_true, y_block.data)
+
+# =============================================================================
+#   assert gradient forward pass
+# =============================================================================
+    assert np.equal(gradient_true, y_block.gradient).all(), 'wrong gradient forward pass. expected {}, given{}'.format(gradient_true,y_block.gradient)
