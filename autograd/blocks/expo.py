85c8dedcaeb9c132234fa66f948bc53705dc295f
Paxton <pmaederyork@gmail.com>
2018-11-09 13:37:37 -0500

    added log and log test

--- autograd/blocks/expo.py
+++ autograd/blocks/expo.py
@@ -15,3 +15,15 @@ class exp(SimpleBlock):
         grad = np.exp(args.data)
         return (grad)
 
+class log(SimpleBlock):
+    """
+    vectorized sinus function on vectors
+    """
+
+    def data_fn(self, args):
+        new_data = np.log(args.data)
+        return (new_data)
+
+    def gradient_fn(self, args):
+        grad = 1/args.data
+        return (grad)
--- autograd/tests/test_expo.py
+++ autograd/tests/test_expo.py
@@ -1,4 +1,5 @@
 from autograd.blocks.expo import exp
+from autograd.blocks.expo import log
 from autograd.variable import Variable
 import numpy as np
 
@@ -34,3 +35,37 @@ def test_exp():
 #   assert gradient forward pass
 # =============================================================================
     assert np.equal(gradient_true, y_block.gradient).all(), 'wrong exp gradient forward pass. expected {}, given{}'.format(gradient_true,y_block.gradient)
+
+
+def test_log():
+# =============================================================================
+#   define the input variable
+# =============================================================================
+    data=np.random.random(5)
+    x=Variable(data)
+
+# =============================================================================
+#   define custom block
+# =============================================================================
+    log_block=log()
+
+# =============================================================================
+#   compute output of custom block
+# =============================================================================
+    y_block=log_block(x)
+
+# =============================================================================
+#   define expected output
+# =============================================================================
+    data_true=np.log(data)
+    gradient_true=np.diag(1/(data))
+
+# =============================================================================
+#   assert data pass
+# =============================================================================
+    assert np.equal(data_true, y_block.data).all(), 'wrong log data pass. expected {}, given{}'.format(data_true, y_block.data)
+
+# =============================================================================
+#   assert gradient forward pass
+# =============================================================================
+    assert np.equal(gradient_true, y_block.gradient).all(), 'wrong log gradient forward pass. expected {}, given{}'.format(gradient_true,y_block.gradient)
