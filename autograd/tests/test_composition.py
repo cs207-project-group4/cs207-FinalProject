b98efb7128123b5aa75069e1fbce0e3bba3e816f
Paxton <pmaederyork@gmail.com>
2018-11-08 20:32:56 -0500

    composition test complete

---
+++ autograd/tests/test_composition.py
@@ -0,0 +1,38 @@
+from autograd.blocks.trigo import sin
+from autograd.blocks.expo import exp
+from autograd.variable import Variable
+import numpy as np
+
+def test_comp():
+# =============================================================================
+#   define the input variable
+# =============================================================================
+    data=np.random.random(5)
+    x=Variable(data)
+
+# =============================================================================
+#   define custom block
+# =============================================================================
+    sin_block = sin()
+    exp_block = exp()
+
+# =============================================================================
+#   compute output of custom block
+# =============================================================================
+    y_block=sin_block(exp_block(x))
+
+# =============================================================================
+#   define expected output
+# =============================================================================
+    data_true=np.sin(np.exp(data))
+    gradient_true=np.exp(data)*np.cos(np.exp(data))*np.identity(5)
+
+# =============================================================================
+#   assert data pass
+# =============================================================================
+    assert np.equal(data_true, y_block.data).all(), 'wrong exp data pass. expected {}, given{}'.format(data_true, y_block.data)
+
+# =============================================================================
+#   assert gradient forward pass
+# =============================================================================
+    assert np.equal(gradient_true, y_block.gradient).all(), 'wrong exp gradient forward pass. expected {}, given{}'.format(gradient_true,y_block.gradient)
