c04ee0eb937b6109da31d17f023b49d89ccabdf9
Paxton <pmaederyork@gmail.com>
2018-11-08 20:20:30 -0500

    added Unary Test

---
+++ autograd/tests/test_unary.py
@@ -0,0 +1,23 @@
+from autograd.blocks.trigo import sin
+from autograd.variable import Variable
+import numpy as np
+
+def test_neg():
+    # =============================================================================
+    #   define the input variable
+    # =============================================================================
+    data = np.random.random(5)
+    x = Variable(data)
+    y = Variable(-data)
+
+    # =============================================================================
+    #   assert data pass
+    # =============================================================================
+    assert np.equal(-x.data, y.data).all(),'Data failed'
+
+    # =============================================================================
+    #   assert gradient forward pass
+    # =============================================================================
+    assert np.equal(x.gradient,y.gradient).all(),'Gradient failed'
+
+test_neg()
