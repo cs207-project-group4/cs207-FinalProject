e673b55c5189a4026fccaa9bc2dc40855811aac5
Dylan Randle <dylanrandle@rogers.com>
2018-12-07 14:28:52 -0500

    remove print

--- autograd/blocks/block.py
+++ autograd/blocks/block.py
@@ -11,28 +11,28 @@ import autograd as ad
 class Block():
     """
     main class for the blocks of the AD package. The several blocks will be of several types
-    For now we envision two types of block : simple and double, depending on the number of 
+    For now we envision two types of block : simple and double, depending on the number of
     variables as input.
-    For instance : 
+    For instance :
         sin(.) is a simple block as it has only one input
-        
+
         dot(.,.) is a double block as it has two inputs
-        
-        
+
+
     NOTE : Maybe we should define the following functions as class methods
     This way we will not have to instantiate these class to use them.
-    for instance instead of doing : 
+    for instance instead of doing :
         import sin
         sinBlock = sin()
         y=sinBlock(x)
-        
-    we could have an implementation like : 
+
+    we could have an implementation like :
         import sin
         y=sin(x)
-    """        
+    """
 
-    
-    
+
+
     def data_fn(self,*args, **kwargs):
         """
         function to apply to the input Variable.
@@ -40,75 +40,74 @@ class Block():
             sin.data_fn(x) will return sin(x)
         """
         raise NotImplementedError
-        
+
     def get_jacobians(self, *args, **kwargs):
         """
         get the jacobians of the current block, evaluated at the input.data point
-        
+
         we specify jacobianS as we want to have the jacobian of the ouput of the block with respect
         to each of the inputs
         """
         raise NotImplementedError
-    
 
-        
+
+
     def gradient_forward(self, *args, **kwargs):
         """
         function implementing the forward pass of the gradient.
-        
+
         Let's consider a computational graph which transforms
         x_0 --> x_1 --> x_2 --> x_3 --> y
-        let's call the output of that block y, then the output of 
+        let's call the output of that block y, then the output of
         gradient_forward(x3), will contain the jacobian of the function x_0 --> y
-    
+
         this function is in charge of pushing the gradients forward, it will combine the
         previously computed gradients to the derivative of this block_function
-        
-        
-        
+
+
+
         this function will depend on wether this is a simple block or a double block.
         for instance :
             sin.gradient_forward(x) will return grad(x) * cos(x)
-            
+
             multiply(x,y) will return : grad(x)*y + x*grad(y)
         """
         """
         (x + y)' = x' + y'
         """
         #operator_check(args)
-        
+
         #concatenate the input gradients
         input_grad = np.concatenate([var.gradient for var in args], axis=0)
-        
+
         #concatenate the jacobians
         jacobians = self.get_jacobians(*args, **kwargs)
         jacobian = np.concatenate([jacob for jacob in jacobians], axis=1)
-       
+
         new_grad = np.matmul(jacobian, input_grad)
         return(new_grad)
-    
+
     def __call__(self, *args, **kwargs):
-        """ 
+        """
         applies the forward pass of the data and the gradient.
         returns a new variable with the updated information on data and gradient.
-        """ 
-        #python circular import fix 
+        """
+        #python circular import fix
         if not 'Variable' in dir():
             from autograd.variable import Variable
-            
-            
+
+
         new_data=self.data_fn(*args, **kwargs)
-        
+
         #in forward mode, we force the flow of gradients with the dats
         if ad.mode=='forward':
-            print(ad.mode)
             new_grad=self.gradient_forward(*args, **kwargs)
-            
-            #in forward mode, we return a full Variable, with gradients
-            return(Variable(new_data, new_grad))    
 
-            
-            
+            #in forward mode, we return a full Variable, with gradients
+            return(Variable(new_data, new_grad))
+
+
+
         else:
             #reverse mode, we will make a forward pass on the data but will store the jacobians
             # we pay attention not to include the Constants in the computational graph
@@ -118,46 +117,46 @@ class Block():
                 if type(arg)==Variable:
                     input_variables+=[arg]
                     variables_indexes+=[index]
-            
+
             children_nodes = [var.node for var in input_variables]
             children_jacs = self.get_jacobians(*args, **kwargs)
-            
+
             #in reverse mode, the Variable does not store the gradients
             outputVariable = Variable(new_data)
-            
-            
+
+
             for i in variables_indexes:
                 outputVariable.node.childrens+=[{'node':children_nodes[i], 'jacobian':children_jacs[i]}]
-                
+
                 #increase the counter
                 children_nodes[i].times_used +=1
-            
+
             return(outputVariable)
 
-            
-                
-            
-        
-    
-        
-    
+
+
+
+
+
+
+
 class SimpleBlock(Block):
     """
-    this block is meant to implement one-variable functions that have been vectorized : 
-        sin(vector) is the vector of coordinates (sin(vector[i])) 
-        
+    this block is meant to implement one-variable functions that have been vectorized :
+        sin(vector) is the vector of coordinates (sin(vector[i]))
+
     For this, these functions are functions from Rn to Rn and they have a Jacobian which is
     a square matrix, with elements only on the diagonal
     """
     def gradient_fn(self, *args, **kwargs):
         """
         function implementing the gradient of data_fn, when it is easy to express
-        for instance : 
+        for instance :
             sin.gradient_fn(x) will return cos(x)
         """
         raise NotImplementedError
-        
-        
+
+
     def get_jacobians(self, *args, **kwargs):
         """
         get the Jacobian matrix of the simple block. It is a diagonal matrix easy to build from the
@@ -167,5 +166,3 @@ class SimpleBlock(Block):
         elts = self.gradient_fn(*args, **kwargs)
         jacobian = np.diag(elts)
         return([jacobian])
-        
-   
