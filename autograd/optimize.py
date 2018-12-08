ab398589842bf24fa6b742f3fd7872075646937b
Dylan Randle <dylanrandle@rogers.com>
2018-12-08 10:43:16 -0500

    add Adam optimizer

--- autograd/optimize.py
+++ autograd/optimize.py
@@ -2,7 +2,7 @@
 # =============================================================================
 # Contains optimization functions for the AD package
 # =============================================================================
-
+import numpy as np
 
 class Optimizer():
     """
@@ -27,17 +27,13 @@ class Optimizer():
 
     def step(self):
         """
-
         Performs a single step of the optimizaiton
-
         """
         raise NotImplementedError
 
     def solve(self):
         """
-
-        loop until tolerance is met (convergence) or for max number of iters
-
+        Loop until convergence criteria is met or for max_iters
         """
         count = 0
         while count < self.max_iter:
@@ -54,7 +50,6 @@ class Optimizer():
 
 
 class GD(Optimizer):
-
     """
     Gradient Descent Optimizer
 
@@ -86,7 +81,36 @@ class GD(Optimizer):
         self.params = self.params - self.lr * grad
 
 
-class SGD(Optimizer):
+class Adam(Optimizer):
+    """
+    Implements Adam Optimizer (`Adam: A Method for Stochastic Optimization`)
+    """
 
-    def __init__(self):
-        pass
+    def __init__(self, *args, beta1=0.9, beta2=0.999, eps=1e-8, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.beta1=beta1
+        self.beta2=beta2
+        self.eps=eps
+        self.exp_avg=np.zeros_like(self.params)
+        self.exp_avg_sq=np.zeros_like(self.params)
+        self.step_count=0
+
+    def step(self):
+        # increment step count
+        self.step_count += 1
+
+        # get current loss
+        loss, grad = self.loss_func(self.params)
+        grad = grad[0]
+
+        # calculate moving averages
+        self.exp_avg = self.exp_avg*self.beta1 + (1-self.beta1)*grad
+        self.exp_avg_sq = self.exp_avg_sq*self.beta2 + (1-self.beta2)*(grad**2)
+
+        # perform bias correction
+        bias_correction1 = self.exp_avg / (1 - self.beta1 ** self.step_count)
+        bias_correction2 = self.exp_avg_sq / (1 - self.beta2 ** self.step_count)
+
+        # calculate step size and perform step
+        step_size = self.lr * bias_correction1 / (np.sqrt(bias_correction2) + self.eps)
+        self.params = self.params - step_size
--- autograd/tests/test_optimize.py
+++ autograd/tests/test_optimize.py
@@ -1,7 +1,7 @@
 import numpy as np
 import autograd as ad
 from autograd.variable import Variable
-from autograd.optimize import GD
+from autograd.optimize import GD, Adam
 
 
 def test_gradient_descent():
@@ -16,4 +16,18 @@ def test_gradient_descent():
     x_init = [10, 4]
     optimize_GD = GD(loss, x_init, lr=0.01, max_iter=100000, tol=1e-18)
     sol = optimize_GD.solve()
+    assert round(sol[0]) == -5 and round(sol[1]) == -3
+
+def test_adam():
+
+    def loss(params):
+        var = Variable(params)
+        x = var[0]
+        y = var[1]
+        l = (x+5)**2 + (y+3)**2
+        return (l.data, l.gradient)
+
+    x_init = [10, 4]
+    adam = Adam(loss, x_init, lr=0.001, max_iter=100000, tol=1e-18)
+    sol = adam.solve()
     assert round(sol[0]) == -5 and round(sol[1]) == -3
