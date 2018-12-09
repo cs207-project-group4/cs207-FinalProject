6f6a238140d302e72dc9ea371a59b2f344c23450
Adam Nitido <anitido@g.harvard.edu>
2018-12-09 15:11:17 -0500

    reverse mode tests

--- autograd/tests/test_expo.py
+++ autograd/tests/test_expo.py
@@ -2,9 +2,11 @@ from autograd.blocks.expo import exp
 from autograd.blocks.expo import log
 from autograd.blocks.expo import sqrt
 from autograd.variable import Variable
+import autograd as ad
 import numpy as np
 
-def test_exp():
+def test_exp_forward():
+    ad.set_mode('forward')
 # =============================================================================
 #   define the input variable
 # =============================================================================
@@ -37,8 +39,44 @@ def test_exp():
 # =============================================================================
     assert np.equal(gradient_true, y_block.gradient).all(), 'wrong exp gradient forward pass. expected {}, given{}'.format(gradient_true,y_block.gradient)
 
+def test_exp_reverse():
+    ad.set_mode('reverse')
+# =============================================================================
+#   define the input variable
+# =============================================================================
+    data=np.random.random(5)
+    x=Variable(data)
 
-def test_log():
+# =============================================================================
+#   define custom block
+# =============================================================================
+    exp_block=exp()
+
+# =============================================================================
+#   compute output of custom block
+# =============================================================================
+    y_block=exp_block(x)
+    y_block.backward()
+
+# =============================================================================
+#   define expected output
+# =============================================================================
+    data_true=np.exp(data)
+    gradient_true=np.diag(np.exp(data))
+
+# =============================================================================
+#   assert data pass
+# =============================================================================
+    assert np.equal(data_true, y_block.data).all(), 'wrong exp data pass. expected {}, given{}'.format(data_true, y_block.data)
+
+# =============================================================================
+#   assert gradient forward pass
+# =============================================================================
+    assert np.equal(gradient_true, y_block.gradient).all(), 'wrong exp gradient forward pass. expected {}, given{}'.format(gradient_true,y_block.gradient)
+    ad.set_mode('forward')
+
+def test_log_forward():
+    ad.set_mode('forward')
 # =============================================================================
 #   define the input variable
 # =============================================================================
@@ -71,7 +109,44 @@ def test_log():
 # =============================================================================
     assert np.equal(gradient_true, y_block.gradient).all(), 'wrong log gradient forward pass. expected {}, given{}'.format(gradient_true,y_block.gradient)
 
-def test_exp():
+def test_log_reverse():
+    ad.set_mode('reverse')
+# =============================================================================
+#   define the input variable
+# =============================================================================
+    data=np.random.random(5)
+    x=Variable(data)
+
+# =============================================================================
+#   define custom block
+# =============================================================================
+    log_block=log()
+
+# =============================================================================
+#   compute output of custom block
+# =============================================================================
+    y_block=log_block(x)
+    y_block.backward()
+
+# =============================================================================
+#   define expected output
+# =============================================================================
+    data_true=np.log(data)
+    gradient_true=np.diag(1/(data))
+
+# =============================================================================
+#   assert data pass
+# =============================================================================
+    assert np.equal(data_true, y_block.data).all(), 'wrong log data pass. expected {}, given{}'.format(data_true, y_block.data)
+
+# =============================================================================
+#   assert gradient forward pass
+# =============================================================================
+    assert np.equal(gradient_true, y_block.gradient).all(), 'wrong log gradient forward pass. expected {}, given{}'.format(gradient_true,y_block.gradient)
+    ad.set_mode('forward')
+
+def test_exp_forward():
+    ad.set_mode('forward')
 # =============================================================================
 #   define the input variable
 # =============================================================================
@@ -103,3 +178,39 @@ def test_exp():
 #   assert gradient forward pass
 # =============================================================================
     assert np.equal(gradient_true, y_block.gradient).all(), 'wrong sqrt gradient forward pass. expected {}, given{}'.format(gradient_true,y_block.gradient)
+
+def test_exp_reverse():
+    ad.set_mode('reverse')
+# =============================================================================
+#   define the input variable
+# =============================================================================
+    data=np.random.random(5)
+    x=Variable(data)
+
+# =============================================================================
+#   define custom block
+# =============================================================================
+    sqrt_block=sqrt()
+
+# =============================================================================
+#   compute output of custom block
+# =============================================================================
+    y_block=sqrt_block(x)
+    y_block.backward()
+
+# =============================================================================
+#   define expected output
+# =============================================================================
+    data_true=np.sqrt(data)
+    gradient_true=np.diag(1/(2*np.sqrt(data)))
+
+# =============================================================================
+#   assert data pass
+# =============================================================================
+    assert np.equal(data_true, y_block.data).all(), 'wrong sqrt data pass. expected {}, given{}'.format(data_true, y_block.data)
+
+# =============================================================================
+#   assert gradient forward pass
+# =============================================================================
+    assert np.equal(gradient_true, y_block.gradient).all(), 'wrong sqrt gradient forward pass. expected {}, given{}'.format(gradient_true,y_block.gradient)
+    ad.set_mode('forward')
--- autograd/tests/test_hyperbolic.py
+++ autograd/tests/test_hyperbolic.py
@@ -75,6 +75,7 @@ def test_sinh_reverse():
 #   assert gradient forward pass
 # =============================================================================
     assert np.equal(gradient_true, y_block.gradient).all(), 'wrong sinh gradient forward pass. expected {}, given{}'.format(gradient_true,y_block.gradient)
+    ad.set_mode('forward')
 
 
 def test_cosh_forward():
@@ -111,6 +112,43 @@ def test_cosh_forward():
 # =============================================================================
     assert np.equal(gradient_true, y_block.gradient).all(), 'wrong cosh gradient forward pass. expected {}, given{}'.format(gradient_true,y_block.gradient)
 
+def test_cosh_reverse():
+    ad.set_mode('reverse')
+# =============================================================================
+#   define the input variablet
+# =============================================================================
+    data=np.random.random(5)
+    x=Variable(data)
+
+# =============================================================================
+#   define custom block
+# =============================================================================
+    cosh_block=cosh()
+
+# =============================================================================
+#   compute output of custom block
+# =============================================================================
+    y_block=cosh_block(x)
+    y_block.backward()
+
+# =============================================================================
+#   define expected output
+# =============================================================================
+    data_true=np.cosh(data)
+    gradient_true=np.diag(np.sinh(data))
+
+# =============================================================================
+#   assert data pass
+# =============================================================================
+    assert np.equal(data_true, y_block.data).all(), 'wrong cosh data pass. expected {}, given{}'.format(data_true, y_block.data)
+
+# =============================================================================
+#   assert gradient forward pass
+# =============================================================================
+    assert np.equal(gradient_true, y_block.gradient).all(), 'wrong cosh gradient forward pass. expected {}, given{}'.format(gradient_true,y_block.gradient)
+    ad.set_mode('forward')
+
+
 def test_tanh_forward():
     ad.set_mode('forward')
 # =============================================================================
@@ -144,3 +182,39 @@ def test_tanh_forward():
 #   assert gradient forward pass
 # =============================================================================
     assert np.equal(gradient_true, y_block.gradient).all(), 'wrong tanh gradient forward pass. expected {}, given{}'.format(gradient_true,y_block.gradient)
+
+def test_tanh_reverse():
+    ad.set_mode('reverse')
+# =============================================================================
+#   define the input variablet
+# =============================================================================
+    data=np.random.random(5)
+    x = Variable(data)
+
+# =============================================================================
+#   define custom block
+# =============================================================================
+    tanh_block=tanh()
+
+# =============================================================================
+#   compute output of custom block
+# =============================================================================
+    y_block=tanh_block(x)
+    y_block.backward()
+
+# =============================================================================
+#   define expected output
+# =============================================================================
+    data_true=np.tanh(data)
+    gradient_true=np.diag(1 - np.tanh(data)**2)
+
+# =============================================================================
+#   assert data pass
+# =============================================================================
+    assert np.equal(data_true, y_block.data).all(), 'wrong tanh data pass. expected {}, given{}'.format(data_true, y_block.data)
+
+# =============================================================================
+#   assert gradient forward pass
+# =============================================================================
+    assert np.equal(gradient_true, y_block.gradient).all(), 'wrong tanh gradient forward pass. expected {}, given{}'.format(gradient_true,y_block.gradient)
+    ad.set_mode('forward')
